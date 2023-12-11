19c1a25e53140586284f9b50c8b320778b14fe8b
silverline-k <riv2rsilv2rlin2@gmail.com>
2023-12-12 01:05:56 +0900

    닉네임 설정 추가

--- src/server.js
+++ src/server.js
@@ -16,15 +16,26 @@ const handleListen = () => console.log('Listening on localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
+const sockets = [];
+
 wss.on('connection', (socket) => {
+    sockets.push(socket);
+    socket['nickname'] = 'Anon';
+
     console.log('Connected to Browser ✅');
 
     socket.on('close', () => console.log('Disconnected from the Browser ❌'));
-    socket.on('message', message => {
-        console.log(message.toString());
-    })
-
-    socket.send('hello!!');
+    socket.on('message', msg => {
+        const message = JSON.parse(msg.toString());
+        switch(message.type) {
+            case 'new_message':
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case 'nickname':
+                socket['nickname'] = message.payload;
+                break;
+        }
+    });
 });
 
 server.listen(3000, handleListen);
